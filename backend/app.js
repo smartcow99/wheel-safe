6a789c77dc6ab6cc464cd0efda3ff21600273a91
_smcow <phw2088@gmail.com>
2022-08-09 18:33:34 +0900

    20220809 backend 구조 업데이트

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,58 @@
 const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+require('dotenv').config();
+
+const indexRouter = require('./routes');
+
 const app = express();
-const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.json({
-    success: true,
-  });
+const sessionMiddleware = session({
+  resave: false,
+  saveUninitialized: false,
+  secret: `${process.env.COOKIE_SECRET}`,
+  cookie: {
+    httpOnly: true,
+    secure: false,
+  },
 });
 
-app.listen(port, () => {
-  console.log(`server is listening at localhost:${port}`);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(logger('dev'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+//catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
 });
+
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+const server = app.listen(app.get('port'), () => {
+  console.log(app.get('port'), '번 포트에서 대기중입니다.');
+});
+
+module.exports = app;
