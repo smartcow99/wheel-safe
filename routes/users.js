20f2865dd40250f45542111c05b07b56fe36b3de
_smcow <phw2088@gmail.com>
2022-08-23 22:05:31 +0900

    전기충전소 위치 구현 완료

--- routes/users.js
+++ routes/users.js
@@ -36,22 +36,20 @@ router.post('/login', async (req, res) => {
   }
 });
 
-
 router.post('/register', async (req, res) => {
   const [isMember] = await db.isMember(req.body.email);
   console.log(isMember);
   console.log(req.body.email, req.body.password);
 
-router.get('/logout', islogined, (req,res)=>{
-    req.session.destroy(function(){
-        req.session;
+  router.get('/logout', islogined, (req, res) => {
+    req.session.destroy(function () {
+      req.session;
     });
     res.clearCookie('email');
     res.status(200).send('logout success');
-    
-})
+  });
 
-router.post('/report', upload.single('image'),async (req, res)=>{
+  router.post('/report', upload.single('image'), async (req, res) => {
     const user = req.cookies.email;
     const title = req.body.title;
     const content = req.body.content;
@@ -59,15 +57,13 @@ router.post('/report', upload.single('image'),async (req, res)=>{
 
     console.log(user, title, content, req.file.filename);
 
-    if(user && title && content){
-        db.report(user,title, content, image);
-        res.status(200).send('report success');
+    if (user && title && content) {
+      db.report(user, title, content, image);
+      res.status(200).send('report success');
+    } else {
+      res.status(401).send('report fail');
     }
-    else{
-        res.status(401).send('report fail');
-    }
-})
-
+  });
 
   if (isMember) {
     res.status(401).send('register fail');
@@ -198,7 +194,6 @@ async function callbackLatLon(fromLat, fromLon, toLat, toLon, num) {
       {
         url: url + qp,
         method: 'GET',
-        timeout: 5000,
       },
 
       function (err, res, body) {
@@ -213,6 +208,7 @@ async function callbackLatLon(fromLat, fromLon, toLat, toLon, num) {
     );
   });
 }
+
 router.get('/poisearch', async (req, res) => {
   let coordinates = [{}];
   let passLists;
@@ -256,9 +252,51 @@ router.get('/poisearch', async (req, res) => {
       passList: passLists,
     },
   };
-  request.post(options, function (err, result, body) {
+  request.post(options, function (err, response, body) {
+    let result = JSON.parse(body);
     res.status(200).send(result);
   });
+});
+
+router.get('/electirc', async (req, res) => {
+  const url =
+    'http://api.data.go.kr/openapi/tn_pubr_public_electr_whlchairhgh_spdchrgr_api';
+  let queryParams =
+    '?' +
+    encodeURIComponent('serviceKey') +
+    '=' +
+    `${process.env.OPEN_KEY}`; /* Service Key*/
+  queryParams +=
+    '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('0'); /* */
+  queryParams +=
+    '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('5'); /* */
+  queryParams +=
+    '&' + encodeURIComponent('type') + '=' + encodeURIComponent('json'); /* */
+  queryParams +=
+    '&' +
+    encodeURIComponent('ctprvnNm') +
+    '=' +
+    encodeURIComponent('서울특별시'); /* */
+  queryParams +=
+    '&' +
+    encodeURIComponent('signguNm') +
+    '=' +
+    encodeURIComponent(`${req.body.signguNm}`); /* */
+
+  request(
+    {
+      url: url + queryParams,
+      method: 'GET',
+    },
+    function (err, response, body) {
+      if (!err && res.statusCode === 200) {
+        resultJson = JSON.parse(body);
+        res.status(200).send(resultJson);
+      } else {
+        console.log(err);
+      }
+    }
+  );
 });
 
 module.exports = router;
