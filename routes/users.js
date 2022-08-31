48b37b62fe8fcfa3c34e36e3e6c646bc1ace3ac1
_smcow <phw2088@gmail.com>
2022-08-31 14:43:52 +0900

    동작구 데이터 추가

--- routes/users.js
+++ routes/users.js
@@ -239,22 +239,84 @@ router.get('/electric', async (req, res) => {
     encodeURIComponent('signguNm') +
     '=' +
     encodeURIComponent(`${signgu}`);
-
-  request(
-    {
-      url: url + queryParams,
-      method: 'GET',
-    },
-    function (err, response, body) {
-      if (!err && res.statusCode === 200) {
-        resultJson = JSON.parse(body);
-        console.log(resultJson);
-        res.status(200).send(resultJson);
-      } else {
-        res.status(401).send('error');
+  console.log(signgu);
+  if (signgu == '동작구') {
+    resultJson = {
+      response: {
+        body: {
+          items: [
+            {
+              fcltyNm: '서울 시립 남부 장애인 종합복지관',
+              rdnmadr: '서울특별시 동작구 신대방동 395번지 보라매공원 내',
+              latitude: '37.490147',
+              longitude: '126.916894',
+            },
+            {
+              fcltyNm: '이수 자이',
+              rdnmadr: '서울특별시 동작구 사당1동 148-16',
+              latitude: '37.4845',
+              longitude: '126.98',
+            },
+            {
+              fcltyNm: '상도 5동 동사무소',
+              rdnmadr: '서울특별시 동작구 상도1동 상도로53길 9 주민센터',
+              latitude: '37.498043',
+              longitude: '126.953090',
+            },
+            {
+              fcltyNm: '총신대학교 종합관',
+              rdnmadr: '서울특별시 동작구 사당로 143',
+              latitude: '37.489732',
+              longitude: '126.966511',
+            },
+            {
+              fcltyNm: '국립 서울 현충 만남의집',
+              rdnmadr:
+                '동작동 산41-2번지 동작동국립서울현충만남의집내서점 동작구 서울특별시 KR',
+              latitude: '37.503194',
+              longitude: '126.969335',
+            },
+            {
+              fcltyNm: '사당역',
+              rdnmadr:
+                'KR 서울특별시 동작구 사당동 588-44번지 지하층 지하3층 사당역4 433동 106호',
+              latitude: '37.476825',
+              longitude: '126.981591',
+            },
+            {
+              fcltyNm: '동작구차근린공원',
+              rdnmadr:
+                '서울특별시 동작구 현충로 220 동작역 9호선 청년창업스튜디오',
+              latitude: '37.503667',
+              longitude: '126.977298',
+            },
+            {
+              fcltyNm: '노량진역 지하 1층',
+              rdnmadr: '서울특별시 동작구 노량진동 60-11 노량진역 지하 1층',
+              latitude: '37.513547',
+              longitude: '126.940842',
+            },
+          ],
+        },
+      },
+    };
+    res.send(resultJson).status(200);
+  } else {
+    request(
+      {
+        url: url + queryParams,
+        method: 'GET',
+      },
+      function (err, response, body) {
+        if (!err && res.statusCode === 200) {
+          resultJson = JSON.parse(body);
+          res.status(200).send(resultJson);
+        } else {
+          res.status(401).send('error');
+        }
       }
-    }
-  );
+    );
+  }
 });
 
 /**
